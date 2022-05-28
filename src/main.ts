c65054f6dab91eec77a64439318b45716bc5ab25
Kaynec <once462@gmail.com>
2022-05-29 02:23:43 +0430

    removed extra service worker

--- src/main.ts
+++ src/main.ts
@@ -47,11 +47,3 @@ app
   .mixin(detectMobile);
 
 app.mount('#app');
-
-// Check that service workers are supported
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
-  });
-}
