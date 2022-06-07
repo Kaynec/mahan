f8766f6baa4f3fde702b4d8f113af11334952ff7
Kaynec <once462@gmail.com>
2022-06-07 17:35:55 +0430

    changing isMobile

--- src/mixins/detectMobile.ts
+++ src/mixins/detectMobile.ts
@@ -1,8 +1,20 @@
+import { useBreakpoints } from '@vueuse/core';
+
+const breakpoints = useBreakpoints({
+  tablet: 640,
+  laptop: 1024,
+  desktop: 1280
+});
+
+const laptop = breakpoints.between('laptop', 'desktop');
+
 const detectMobile = {
   methods: {
     isMobile: function (): boolean {
-      const check = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-      return check;
+      // const check = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+      // return check;
+      console.log(breakpoints.smaller('tablet').value);
+      return breakpoints.smaller('tablet').value;
     }
   }
 };
