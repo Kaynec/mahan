1cd6200c3a1541166d5df26905c3107ca10a0c82
Kaynec <once462@gmail.com>
2022-06-30 09:19:01 +0430

    trying to fix pwas

--- src/main.ts
+++ src/main.ts
@@ -44,3 +44,8 @@ app
   .use(router);
 
 app.mount('#app');
+
+router.isReady().then(async () => {
+  const { registerSW } = await import('virtual:pwa-register')
+  registerSW({ immediate: true })
+})
